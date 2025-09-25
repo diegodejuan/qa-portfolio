6be229747f186e45fec1a963ae1873246e62dae7
Diego <diegodejuan2@gmail.com>
2025-09-25 18:41:16 +0200

    se añade caso de prueba GET /users lista de usuarios

--- tests-api/tests/login.spec.ts
+++ tests-api/tests/login.spec.ts
@@ -1,11 +1,32 @@
 import { test, expect } from "@playwright/test";
 import users from "../fixtures/users.json";
 
+test("GET /users válido → 200 OK + verificación array respuesta", async ({ request }) => {
+    const response = await request.get("/api/users?page=2");
+    // Status esperado
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    // Validamos que contenga la propiedad data
+    expect(body).toHaveProperty("data");
+    // Validamos que el array data no esté vacío
+    expect(Array.isArray(body.data)).toBe(true);
+    expect(body.data.length).toBeGreaterThan(0);
+
+    // Validamos que cada usuario tenga id, email, first_name, last_name y avatar
+    for (const user of body.data) {
+        expect(user).toHaveProperty("id");
+        expect(user).toHaveProperty("email");
+        expect(user).toHaveProperty("first_name");
+        expect(user).toHaveProperty("last_name");
+        expect(user).toHaveProperty("avatar");
+    }
+});
+
 test("POST /login válido → 200 OK + token no vacío", async ({ request }) => {
     const headers = {
         "x-api-key": "reqres-free-v1"
     };
-    
+
     const response = await request.post("/api/login", {
         data: users.validUser,
         headers: headers
@@ -23,3 +44,22 @@ test("POST /login válido → 200 OK + token no vacío", async ({ request }) => 
     expect(typeof body.token).toBe("string");
     expect(body.token.length).toBeGreaterThan(0);
 });
+
+test("POST /login sin password → 400 Bad Request", async ({ request }) => {
+    const headers = {
+        "x-api-key": "reqres-free-v1"
+    };
+
+    const response = await request.post("/api/login", {
+        data: { email: users.validUser.email },
+        headers: headers
+    });
+
+    expect(response.status()).toBe(400);
+
+    const body = await response.json();
+
+    // Validamos mensaje de error exacto
+    expect(body).toMatchObject({ error: "Missing password" });
+});
+
