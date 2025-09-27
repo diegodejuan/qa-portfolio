d08177f548b2a8aed63b3f5c51bc9c30b1b22a5f
Diego <diegodejuan2@gmail.com>
2025-09-27 17:45:22 +0200

    formateo de documento

--- tests-api/tests/users.spec.ts
+++ tests-api/tests/users.spec.ts
@@ -30,28 +30,28 @@ test("GET /users?page=2 válido → 200 OK + verificación array respuesta", asy
 });
 
 test("GET /users?page=2 → validación de paginación", async ({ request }) => {
-  const page = 2;
-  const apiClient = new ApiClient(request);
-  const response = await apiClient.getUsers(page);
+    const page = 2;
+    const apiClient = new ApiClient(request);
+    const response = await apiClient.getUsers(page);
 
-  expect(response.status()).toBe(200);
+    expect(response.status()).toBe(200);
 
-  const body = await response.json();
+    const body = await response.json();
 
-  //Validamos metadatos de paginación
-  expect(body).toHaveProperty("page", page);
-  expect(body).toHaveProperty("per_page");
-  expect(body).toHaveProperty("total");
-  expect(body).toHaveProperty("total_pages");
+    //Validamos metadatos de paginación
+    expect(body).toHaveProperty("page", page);
+    expect(body).toHaveProperty("per_page");
+    expect(body).toHaveProperty("total");
+    expect(body).toHaveProperty("total_pages");
 
-  //Reglas básicas de consistencia
-  expect(body.per_page).toBeGreaterThan(0);
-  expect(body.total_pages * body.per_page).toBeGreaterThanOrEqual(body.total);
-  expect(body.data.length).toBeLessThanOrEqual(body.per_page);
+    //Reglas básicas de consistencia
+    expect(body.per_page).toBeGreaterThan(0);
+    expect(body.total_pages * body.per_page).toBeGreaterThanOrEqual(body.total);
+    expect(body.data.length).toBeLessThanOrEqual(body.per_page);
 
-  //Validamos que data sea array y no esté vacío
-  expect(Array.isArray(body.data)).toBe(true);
-  expect(body.data.length).toBeGreaterThan(0);
+    //Validamos que data sea array y no esté vacío
+    expect(Array.isArray(body.data)).toBe(true);
+    expect(body.data.length).toBeGreaterThan(0);
 });
 
 test("GET /users/23 → 404 Usuario no existe", async ({ request }) => {
@@ -63,7 +63,7 @@ test("GET /users/23 → 404 Usuario no existe", async ({ request }) => {
 
     //Validamos el status code
     expect(response.status()).toBe(404);
-    
+
     //La API devuelve un objeto vacío
     expect(await response.json()).toEqual({});
 
