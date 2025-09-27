36559e44cbc2380248b01136960ebb8138fdaf28
Diego <diegodejuan2@gmail.com>
2025-09-27 17:32:52 +0200

    refactorizacion - se colocan test de usuarios en users.spec.ts

--- tests-api/tests/login.spec.ts
+++ tests-api/tests/login.spec.ts
@@ -1,34 +1,6 @@
 import { test, expect } from "@playwright/test";
 import users from "../fixtures/users.json";
-import { ApiClient } from "../apiClient";
-
-test("GET /users válido → 200 OK + verificación array respuesta", async ({ request }) => {
-    const apiClient = new ApiClient(request);
-    const response = await apiClient.getUsers(2);
-    
-    // Status esperado
-    expect(response.status()).toBe(200);
-
-    const body = await response.json();
-
-    // Validamos que contenga la propiedad data
-    expect(body).toHaveProperty("data");
-
-    //Validamos que body.data es un array
-    expect(Array.isArray(body.data)).toBe(true);
-
-    // Validamos que el array data no esté vacío
-    expect(body.data.length).toBeGreaterThan(0);
-
-    // Validamos que cada usuario tenga id, email, first_name, last_name y avatar
-    for (const user of body.data) {
-        expect(user).toHaveProperty("id");
-        expect(user).toHaveProperty("email");
-        expect(user).toHaveProperty("first_name");
-        expect(user).toHaveProperty("last_name");
-        expect(user).toHaveProperty("avatar");
-    }
-});
+import { ApiClient } from "../ApiClient";
 
 test("POST /login válido → 200 OK + token no vacío", async ({ request }) => {
     const apiClient = new ApiClient(request);
